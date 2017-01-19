d35e427c67b173284884df5dbf8080b8424cdae2
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-01-19 16:45:16 +0100

    Added basic logic to ConectionProvider class. Need more improvement.

--- src/dao/ConnectionProvider.java
+++ src/dao/ConnectionProvider.java
@@ -1,10 +1,32 @@
 package dao;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Created by Krzysztof on 2017-01-18.
  */
 public class ConnectionProvider {
 
+    private static DataSource dataSource;
 
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection("root","root");
+    }
+
+    public static DataSource getInstance() throws NamingException {
+
+        if (dataSource == null){
+            Context initialContext = new InitialContext();
+            Context envContext = (Context) initialContext.lookup("java:comp/env");
+            dataSource = (DataSource) envContext.lookup("jdbc/chat_db");
+        }
+
+        return dataSource;
+    }
 
 }
