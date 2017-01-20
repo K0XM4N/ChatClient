b84576d82a5dee14edb8a0e360b7704585d81b61
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-01-20 13:09:38 +0100

    Added pop up messages (verify handling) to LoginFieldsValidatorService.

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,18 +1,22 @@
 package dao;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import lombok.AllArgsConstructor;
+import service.AlertService;
 import service.LoginFieldsValidatorService;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by Krzysztof on 2017-01-19.
  */
-@AllArgsConstructor
+
 public class UserDAO {
 
     private TextField loginInput, usernameInput;
@@ -21,13 +25,36 @@ public class UserDAO {
     private static final String CREATE = "INSERT INTO user( login, username, password) VALUES(?, ?, ?, ?);";
     private static final String READ = "SELECT password FROM user WHERE login = ?;";
 
+    private String login;
+    private String username;
+    private String pass1;
+    private String pass2;
 
-    public void createUser(){
+
+    public UserDAO(TextField loginInput,TextField usernameInput, PasswordField pass1Input, PasswordField pass2Input){
+        this.loginInput = loginInput;
+        this.usernameInput = usernameInput;
+        this.pass1Input = pass1Input;
+        this.pass2Input = pass2Input;
 
         String login = loginInput.getText();
         String username = usernameInput.getText();
         String pass1 = pass1Input.getText();
         String pass2 = pass2Input.getText();
+    }
+
+    public UserDAO(TextField loginInput,TextField usernameInput, PasswordField pass1Input){
+        this.loginInput = loginInput;
+        this.usernameInput = usernameInput;
+        this.pass1Input = pass1Input;
+        this.pass2Input = pass2Input;
+
+        String login = loginInput.getText();
+        String username = usernameInput.getText();
+        String pass1 = pass1Input.getText();
+    }
+
+    public void createUser(){
 
         if (LoginFieldsValidatorService.isFieldVerified(login,username,pass1,pass2)){
 
@@ -39,13 +66,29 @@ public class UserDAO {
 
                 connection = ConnectionProvider.getConnection();
                 sqlStatement = connection.prepareStatement(CREATE);
-                sqlStatement.setString(1, "");
+                sqlStatement.setString(1, login);
 
-            } catch (Exception ex){
+                // read from database, check login -> if there is no login, create user.
 
+            } catch (SQLException ex){
+                AlertService.showAlert(Alert.AlertType.WARNING, "Database error", "Cannot connect do database");
+            } catch (PropertyVetoException e) {
+                e.printStackTrace();
             }
 
         }
+
+    }
+
+    public void selectUserLoginFromDB(){
+
+        if (LoginFieldsValidatorService.isFieldVerified(login,username,pass1)){
+
+        }
+
+    }
+
+    public void selectUserRegisterFromDB(){
 
     }
 
--- src/service/LoginFieldsValidatorService.java
+++ src/service/LoginFieldsValidatorService.java
@@ -1,5 +1,7 @@
 package service;
 
+import javafx.scene.control.Alert;
+
 /**
  * Created by Krzysztof on 2017-01-19.
  */
@@ -8,11 +10,26 @@ public class LoginFieldsValidatorService {
     public static boolean isFieldVerified(String login, String username, String pass1, String pass2){
 
         if (login.isEmpty() || username.isEmpty() || pass1.isEmpty() || pass2.isEmpty()){
+            AlertService.showAlert(Alert.AlertType.INFORMATION, "Empty field","Please fill all fields.");
             return false;
         }
         else{
             if (pass1.equals(pass2)) return true;
-            else return  false;
+            else{
+                AlertService.showAlert(Alert.AlertType.ERROR,"Invalid password","Passwords are not the same!");
+                return  false;
+            }
+        }
+
+    }
+
+    public static boolean isFieldVerified(String login, String username, String pass1){
+
+        if (login.isEmpty() || username.isEmpty() || pass1.isEmpty()){
+            return false;
+        }
+        else{
+            return true;
         }
 
     }
